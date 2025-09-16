f6fb1b7b84de04bcedf22401c6f2b10b416303e1
Divya Kapoor <38455002+divyakapoor23@users.noreply.github.com>
2025-09-16 21:30:04 +0000

    fixed errors and added comments

--- apputil.py
+++ apputil.py
@@ -2,7 +2,14 @@
 import pandas as pd
 import numpy as np  
 
-## function 1
+# Load the Bellevue Almshouse dataset
+url = 'https://github.com/melaniewalsh/Intro-Cultural-Analytics/raw/master/book/data/bellevue_almshouse_modified.csv'
+
+df_bellevue = pd.read_csv(url)
+df_bellevue.head()
+
+
+"Fibonacci sequence starting with 0 and 1"
 def fib(n):
     if n <= 0:
         return 0
@@ -11,51 +18,49 @@ def fib(n):
     else:
         # Recursive case
         return fib(n-1) + fib(n-2)
+print(fib(10))
 
-## function 2
+"Convert a decimal number to its binary representation"
 def to_binary(n):
-    """Convert a decimal number to its binary representation."""
     if n < 0:
         return "Input should be a non-negative integer."
     # Using Python's built-in bin function to convert decimal to binary
     # and remove the '0b' prefix
     return bin(n).replace("0b", "")
+print(to_binary(10))
 
+"Data Analysis on Bellevue Hospital Admissions"
 
-url = 'https://github.com/melaniewalsh/Intro-Cultural-Analytics/raw/master/book/data/bellevue_almshouse_modified.csv'
-
-df_bellevue = pd.read_csv(url)
-df_bellevue.head()
-
-## function 4
+"Return a list of column names sorted by the number of missing values in ascending order"
 def taski():
     missing_count = df_bellevue.isnull().sum()
     sorted_columns = missing_count.sort_values().index.tolist()
     print(sorted_columns)
 
 
+"Calculate the total number of admissions per year and return a DataFrame with 'Year' and 'Total_Admissions'"
 def taskii():
-    # Suppose your date column is named 'date'
-    # df_bellevue['date_in'] = pd.to_datetime(df_bellevue['date_in'], errors='coerce')
-    # df_bellevue['year'] = df_bellevue['date_in'].dt.year
-    # df_bellevue['month'] = df_bellevue['date_in'].dt.month
-    # df_bellevue['day'] = df_bellevue['date_in'].dt.day
-    # print(df_bellevue[['date', 'year', 'month', 'day']].head())
-    # df_bellevue['date_in'] = pd.to_datetime(df_bellevue['date_in'])
+    
+    df_bellevue['date_in'] = pd.to_datetime(df_bellevue['date_in'], errors='coerce')
     df_bellevue['Year'] = df_bellevue['date_in'].dt.year
     df_yearly = df_bellevue.groupby('Year').size().reset_index(name='Total_Admissions')
     print(df_yearly)
 
+
+"Calculate the average age of patients grouped by gender"
 def taskiii():
     avg_age_by_gender = df_bellevue.groupby('gender')['age'].mean().reset_index(name='avg_age')
     print(avg_age_by_gender)
 
+
+"Identify the top 5 most common professions among the patients"
 def taskiv():
+    # 
     top5_professions = df_bellevue['profession'].value_counts().head(5).index.tolist()
     print(top5_professions)
 
 
-taski()
-taskii()
-# taskiii()
-# taskiv()
+print(taski())
+print(taskii())
+print(taskiii())
+print(taskiv())
